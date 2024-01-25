2e319cb86fbf6e3eb13fe69f403173dc129da8f0
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-01-25 20:09:17 +0300

    Update screap.js

--- src/pages/utils/screap.js
+++ src/pages/utils/screap.js
@@ -8,8 +8,13 @@ const fetchFullness = async () => {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
     $('div:contains("Şuanki Kullanım Doluluğu")').each((index, element) => {
-      console.log($(element).text());
+      const text = $(element).text();
+      const usage = text.split(':')[1].split('Toplam')[0].trim();
+      console.log(`Şu anki doluluk: ${usage}`);
+      const totalCapacity = text.split(':')[2].split('Doluluk')[0].trim();
+      console.log(`Max doluluk: ${totalCapacity}`)
     });
+
   } catch (error) {
     console.log("Error", error);
   }
