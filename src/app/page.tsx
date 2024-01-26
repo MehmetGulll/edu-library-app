d5a911cce85fbde3101b47df2d5bccb4c41df4d3
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2024-01-27 00:11:04 +0300

    fix: remove console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { getOccupancyByDateRange } from "../../utils/getOccupancy"
 
 export default async function Home() {
   const occupancyArr = await getOccupancyByDateRange("23.01.2024", "26.01.2024")
-  console.log("🚀 ~ Home ~ occupancyArr:", occupancyArr)
   return (
     <div className="flex justify-center p-8">
       <div className="flex container gap-2">
