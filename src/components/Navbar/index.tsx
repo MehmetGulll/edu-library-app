d373d7711f33c958002450fe4bb13bb70a1c48a3
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-02-05 10:58:19 +0300

    Update: index.tsx-hover effect

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-silver_lake_blue-300 flex flex-row justify-end space-x-8 p-6">
-      <a href="https://debis.deu.edu.tr/debis.php" target="_blank" className="text-white">
+      <a href="https://debis.deu.edu.tr/debis.php" target="_blank" className="text-white hover:text-black transition-colors">
         Debis Giriş
       </a>
-      <a href="https://online.deu.edu.tr/" target="_blank" className="text-white">
+      <a href="https://online.deu.edu.tr/" target="_blank" className="text-white hover:text-black transition-colors">
         Sakai Giriş
       </a>
-      <a href="https://webmail7.deu.edu.tr/" target="_blank" className="text-white">
+      <a href="https://webmail7.deu.edu.tr/" target="_blank" className="text-white hover:text-black transition-colors">
         Mail Giriş
       </a>
     </div>
