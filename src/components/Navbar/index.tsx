04466a0bce03a89b1dbba74b46ba1bfaabeecf06
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-03-09 14:25:09 +0300

    Update: index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,14 +3,33 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-silver_lake_blue-300 flex flex-row justify-end space-x-8 p-6">
-      <a href="https://debis.deu.edu.tr/debis.php" target="_blank" className="text-white hover:text-black transition-colors">
+      <a
+        href="https://debis.deu.edu.tr/debis.php"
+        target="_blank"
+        className="text-white hover:text-black transition-colors"
+      >
         Debis Giriş
       </a>
-      <a href="https://online.deu.edu.tr/" target="_blank" className="text-white hover:text-black transition-colors">
+      <a
+        href="https://online.deu.edu.tr/"
+        target="_blank"
+        className="text-white hover:text-black transition-colors"
+      >
         Sakai Giriş
       </a>
-      <a href="https://webmail7.deu.edu.tr/" target="_blank" className="text-white hover:text-black transition-colors">
+      <a
+        href="https://webmail7.deu.edu.tr/"
+        target="_blank"
+        className="text-white hover:text-black transition-colors"
+      >
         Mail Giriş
+      </a>
+      <a
+        href="https://kutuphane.deu.edu.tr/tr/category/duyurular/"
+        target="_blank"
+        className="text-white hover:text-black transition-colors"
+      >
+        Duyurular
       </a>
     </div>
   );
