d7bb596ee151b2cbcecc630e29d71dc366ae1f51
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2024-03-15 20:33:04 +0300

    feat: typegen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import DateCard from '@/components/DateCard';
 import Chart from '@/components/Chart';
 import { getOccupancyByDateRange } from '../../utils/getOccupancy';
+import { getProductsByCategory } from '@/api';
+import { useGetBorrowQuery } from '@/generated/graphql';
 
 export default async function Home() {
   const occupancyArr = await getOccupancyByDateRange(
     '23.01.2024',
     '26.01.2024'
+  );
+
+  const data = await getProductsByCategory();
+
+  const groupedByDate = data.borrow.reduce(
+    (acc: { [key: string]: any[] }, item) => {
+      if (!acc[item.date as keyof typeof acc]) {
+        acc[item.date] = [];
+      }
+      acc[item.date].push(item);
+      return acc;
+    },
+    {}
   );
 
   return (
