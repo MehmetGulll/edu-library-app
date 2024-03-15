24b059227c9d364a1b2734b031307e978a6d1402
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2024-03-15 22:58:38 +0300

    fix: style fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,40 +1,56 @@
-import React from 'react';
+import React from "react";
+
+interface FooterItemProps {
+  title: string;
+  content: string;
+  link?: "tel" | "mailto" | "map";
+}
+
+const FooterItem = ({ title, content, link }: FooterItemProps) => {
+  return (
+    <div>
+      <div className='font-bold text-gray-800'>{title}</div>
+      <div className='text-sm text-gray-500'>
+        {link ? <a href={link + ":" + content}>{content}</a> : content}
+      </div>
+    </div>
+  );
+};
 
 const Footer = () => {
+  const footerItems: FooterItemProps[] = [
+    {
+      title: "Kütüphane Dokümantasyon Daire Başkanlığı",
+      content:
+        "Prof.Dr. Fuat Sezgin Merkez Kütüphanesi No:207/R D.E.Ü. Tınaztepe Kampüsü 35390 Doğuş Caddesi Buca – İzmir",
+    },
+    {
+      title: "Telefon",
+      content: "+90 232 301 80 17",
+
+      link: "tel",
+    },
+    {
+      title: "Mail Adres",
+      content: "kutuphane@deu.edu.tr",
+      link: "mailto",
+    },
+  ];
+
+  const footerElements = footerItems.map((item, index) => (
+    <FooterItem key={index} {...item} />
+  ));
+
   return (
-    <div className='space-x-8 bg-silver_lake_blue-300 p-6'>
-      <div className='flex flex-col space-y-4'>
-        <div className='flex flex-col'>
-          <div className='font-bold text-white '>
-            Kütüphane Dokümantasyon Daire Başkanlığı
-          </div>
-          <a
-            href='https://www.google.com/maps/place/Dokuz+Eyl%C3%BCl+%C3%9Cniversitesi+Prof.+Dr.+Fuat+Sezgin+Merkez+K%C3%BCt%C3%BCphanesi/@38.3702639,27.2025508,15z/data=!4m6!3m5!1s0x14b9618bf9f8473f:0x12299c8ca3ab3507!8m2!3d38.3702639!4d27.2025508!16s%2Fg%2F1td1y7_m?entry=ttu'
-            className='text-sm text-white'
-            target='_blank'
-          >
-            Prof.Dr. Fuat Sezgin Merkez Kütüphanesi No:207/R D.E.Ü. Tınaztepe
-            Kampüsü 35390 Doğuş Caddesi Buca – İzmir
-          </a>
-        </div>
-        <div className='flex flex-col'>
-          <div className='font-bold text-white'>Telefon</div>
-          <div className='text-sm text-white'>0 232 301 80 17</div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='font-bold text-white'>Mail Adres</div>
-          <div className='text-sm text-white'>kutuphane@deu.edu.tr</div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='font-bold text-white'>Konum</div>
-          <a
-            href='https://tinyurl.com/deukutuphane'
-            className='text-sm text-white'
-          >
-            https://tinyurl.com/deukutuphane
-          </a>
-        </div>
-      </div>
+    <div className='from-light_blue-900 to-light_blue-700 border-t-light_blue-600 flex w-full flex-col gap-8 border-t bg-gradient-to-r p-4 sm:flex-row sm:p-6'>
+      <div className=' flex w-full flex-col gap-4 '>{footerElements}</div>
+      <iframe
+        src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12512.321180630806!2d27.2025508!3d38.3702639!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b9618bf9f8473f%3A0x12299c8ca3ab3507!2sDokuz%20Eyl%C3%BCl%20%C3%9Cniversitesi%20Prof.%20Dr.%20Fuat%20Sezgin%20Merkez%20K%C3%BCt%C3%BCphanesi!5e0!3m2!1str!2str!4v1710524654495!5m2!1str!2str'
+        width='350px'
+        height='350px'
+        className='w-full rounded-lg shadow-lg sm:h-[350px] sm:min-h-[350px] sm:w-[350px] sm:min-w-[350px]'
+        loading='lazy'
+      ></iframe>
     </div>
   );
 };
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,32 +5,32 @@ const Navbar = () => {
   return (
     <div>
       <OpenWeather />
-      <div className='flex flex-row justify-end space-x-8 bg-silver_lake_blue-300 p-6'>
+      <div className='bg-light_blue-900 flex flex-row justify-end space-x-8 border-y p-6'>
         <a
           href='https://debis.deu.edu.tr/debis.php'
           target='_blank'
-          className='text-white transition-colors hover:text-black'
+          className='font-semibold text-gray-800 transition-colors hover:text-black'
         >
           Debis Giriş
         </a>
         <a
           href='https://online.deu.edu.tr/'
           target='_blank'
-          className='text-white transition-colors hover:text-black'
+          className='font-semibold text-gray-800 transition-colors hover:text-black'
         >
           Sakai Giriş
         </a>
         <a
           href='https://webmail7.deu.edu.tr/'
           target='_blank'
-          className='text-white transition-colors hover:text-black'
+          className='font-semibold text-gray-800 transition-colors hover:text-black'
         >
           Mail Giriş
         </a>
         <a
           href='https://kutuphane.deu.edu.tr/tr/category/duyurular/'
           target='_blank'
-          className='text-white transition-colors hover:text-black'
+          className='font-semibold text-gray-800 transition-colors hover:text-black'
         >
           Duyurular
         </a>
