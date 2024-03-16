4cfa23d3e9c9ad9b5f683b99439c79b35606a0d4
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2024-03-17 02:13:23 +0300

    feat: announcements and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,83 @@
 import DateCard from "@/components/DateCard";
 import Chart from "@/components/Chart";
-import { getOccupancyByDateRange } from "../../utils/getOccupancy";
+import { OccupancyWithDate } from "../../utils/getOccupancy";
 import { getLibraryData } from "../../utils/getLibraryData";
+import React from "react";
+import AnnouncementList from "@/components/AnnouncementList";
 
 export default async function Home() {
-  const occupancyArr = await getOccupancyByDateRange(
-    "23.01.2024",
-    "26.01.2024"
-  );
+  // const occupancyArr = await getOccupancyByDateRange(
+  //   "23.01.2024",
+  //   "26.01.2024"
+  // );
+
+  const occupancyArr: OccupancyWithDate[] = [
+    {
+      date: "23.01.2024",
+      occupancy: {
+        total: "100",
+        currentOccupancy: "50",
+        capacity: "100",
+      },
+    },
+    {
+      date: "24.01.2024",
+      occupancy: {
+        total: "100",
+        currentOccupancy: "60",
+        capacity: "100",
+      },
+    },
+    {
+      date: "25.01.2024",
+      occupancy: {
+        total: "100",
+        currentOccupancy: "70",
+        capacity: "100",
+      },
+    },
+    {
+      date: "26.01.2024",
+      occupancy: {
+        total: "100",
+        currentOccupancy: "80",
+        capacity: "100",
+      },
+    },
+  ];
 
   const { dates, counts, categoryCounts } = await getLibraryData();
 
   return (
     <div className='p-4'>
-      <div className=' mb-8 flex flex-col justify-center gap-4 rounded-lg border border-gray-300 p-8 shadow-md'>
-        <div className='flex w-full'>
-          <div className='w-1/2'>
+      <div className='mb-8 flex flex-col justify-center gap-4 rounded-lg border border-gray-300 p-8 shadow-md'>
+        <div className='flex w-full flex-wrap gap-2'>
+          <div className='w-full rounded-xl border border-gray-300 p-2 shadow-md lg:w-[calc(66%-8px)]'>
             <Chart
               height={350}
               type='line'
               options={{
                 colors: ["#49dcb1", "#000000"],
+                title: {
+                  text: "Ödünç Kitap Sayısı ve Toplam Doluluk",
+                  align: "center",
+                  style: {
+                    color: "#e27396",
+                    fontSize: "14px",
+                  },
+                },
+                responsive: [
+                  {
+                    breakpoint: 640,
+                    options: {
+                      title: {
+                        style: {
+                          fontSize: "12px",
+                        },
+                      },
+                    },
+                  },
+                ],
                 xaxis: {
                   categories: dates,
                   tooltip: {
@@ -33,13 +90,13 @@ export default async function Home() {
                 yaxis: [
                   {
                     title: {
-                      text: "Total Occupancy",
+                      text: "Toplam Doluluk",
                     },
                   },
                   {
                     opposite: true,
                     title: {
-                      text: "Total Borrow Books",
+                      text: "Ödünç Kitap Sayısı",
                     },
                   },
                 ],
@@ -49,11 +106,11 @@ export default async function Home() {
               }}
               series={[
                 {
-                  name: "Total Rent Books ",
+                  name: "Ödünç Kitap Sayısı",
                   data: counts,
                 },
                 {
-                  name: "Total Occupancy",
+                  name: "Toplam Doluluk",
                   data: occupancyArr.map((item) =>
                     Number(item.occupancy.total)
                   ),
@@ -62,24 +119,50 @@ export default async function Home() {
               width='100%'
             />
           </div>
-          <div className='w-1/2'>
+          <div className='w-full rounded-xl border border-gray-300 p-2 shadow-md lg:w-1/3'>
             <Chart
               width='100%'
               height={350}
               type='pie'
               options={{
                 labels: categoryCounts.map((item) => item.name),
+                title: {
+                  text: "Ödünç Alınan Kitapların Kategori Dağılımı",
+                  align: "center",
+                  margin: 20,
+                  offsetY: -10,
+                  style: {
+                    color: "#e27396",
+                    fontSize: "14px",
+                  },
+                },
+                responsive: [
+                  {
+                    breakpoint: 640,
+                    options: {
+                      title: {
+                        style: {
+                          fontSize: "10px",
+                        },
+                      },
+                    },
+                  },
+                ],
+                legend: {
+                  show: false,
+                },
               }}
               series={categoryCounts.map((item) => item.data)}
             />
           </div>
         </div>
+        <div className='container -m-2 flex scroll-pl-2 gap-2 overflow-x-auto p-2'>
+          {occupancyArr.map((item) => (
+            <DateCard {...item} key={item.date} />
+          ))}
+        </div>
       </div>
-      <div className='container flex gap-2'>
-        {occupancyArr.map((item) => (
-          <DateCard {...item} key={item.date} />
-        ))}
-      </div>
+      <AnnouncementList />
     </div>
   );
 }
---
+++ utils/getLatestAnnouncements.ts
@@ -0,0 +1,58 @@
+import { load } from "cheerio";
+const getLatestAnnouncements = async () => {
+  const response = await fetch(
+    "https://kutuphane.deu.edu.tr/tr/category/duyuru-arsivi/"
+  );
+  const $ = load(await response.text());
+
+  const titles: string[] = [];
+  $(".no-top").each((i, e) => {
+    titles.push($(e).text().trim());
+  });
+
+  const descriptions: string[] = [];
+  $(".description").each((i, e) => {
+    descriptions.push($(e).text().trim().split("dakika")[1]);
+  });
+
+  const links: string[] = [];
+  $(".read-more").each((i, e) => {
+    links.push($(e).attr("href") ?? "");
+  });
+
+  const content = links.map(async (link, i) => {
+    const response = await fetch(link);
+    const $ = load(await response.text());
+    const article = $("article");
+    const date = article
+      .find(".date")
+      .text()
+      .trim()
+      .split(": ")[1]
+      .replaceAll("-", ".");
+    // grab all html after .post-modified-info
+    const content = article.find(".post-modified-info").nextAll().toString();
+    // change all anchor tags to open in new tab
+    const newContent = content.replace(/<a /g, '<a target="_blank" ');
+
+    return { date, content: newContent };
+  });
+
+  const contents = await Promise.all(content);
+
+  const announcements = titles.map((title, i) => ({
+    title,
+    description: descriptions[i],
+    link: links[i],
+    date: contents[i].date,
+    content: contents[i].content,
+  }));
+
+  return {
+    titles,
+    descriptions,
+    links,
+    announcements,
+  };
+};
+export default getLatestAnnouncements;
