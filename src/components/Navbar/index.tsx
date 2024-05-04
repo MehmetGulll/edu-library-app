756e4ce08763f91577a915b485183418e1557a66
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-05-04 20:11:31 +0300

    feat: hourly get occupancy in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import OpenWeather from "../OpenWeather";
 import WorkingHours from "../WorkingHours";
+import OccupancyCard from "../OccupancyCard"; 
 
 const Navbar = () => {
   return (
@@ -8,19 +9,24 @@ const Navbar = () => {
       <div className='px-4 pt-1'>
         <OpenWeather />
       </div>
-      <div className='flex w-full flex-row items-center justify-end gap-4 border-y bg-light_blue-900 px-4 py-6'>
+      <div className='flex w-full flex-row items-center justify-between gap-4 border-y bg-light_blue-900 px-4 py-6'> 
         <div className='rounded-lg bg-rose_pompadour-500  text-white transition-colors hover:bg-rose_pompadour-400'>
-          <WorkingHours />
+          <OccupancyCard /> 
         </div>
-
-        <a
-          href='#announcements'
-          className='whitespace-nowrap text-sm font-semibold text-light_blue-300 transition-colors hover:text-light_blue-200 sm:text-base'
-        >
-          Duyurular
-        </a>
+        <div className='flex items-center gap-4'> 
+          <div className='rounded-lg bg-rose_pompadour-500  text-white transition-colors hover:bg-rose_pompadour-400'>
+            <WorkingHours />
+          </div>
+          <a
+            href='#announcements'
+            className='whitespace-nowrap text-sm font-semibold text-light_blue-300 transition-colors hover:text-light_blue-200 sm:text-base'
+          >
+            Duyurular
+          </a>
+        </div>
       </div>
     </div>
   );
 };
+
 export default Navbar;
---
+++ src/components/OccupancyCard/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { getOccupancy } from "../../../utils/getOccupancy";
+
+type Occupancy = {
+  total: string
+  currentOccupancy: string
+  capacity: string
+}
+
+const OccupancyCard = () => {
+  const [occupancy, setOccupancy] = useState<Occupancy>({ total: "0", currentOccupancy: "0", capacity: "0" });
+
+  const formatDate = (date: Date): `${number}.${number}.${number}` => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1; 
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}` as `${number}.${number}.${number}`;
+  };
+
+  useEffect(() => {
+    const fetchOccupancy = async () => {
+      const date = formatDate(new Date());
+      const occupancy = await getOccupancy(date);
+      setOccupancy(occupancy);
+    };
+
+    fetchOccupancy();
+    const intervalId = setInterval(fetchOccupancy, 60 *1000); 
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  return (
+    <div className='rounded-xl p-2 px-4 shadow-md outline outline-1 outline-gray-300'>
+      <div className='mb-2 flex flex-col gap-1'>
+        <div className='text-lg font-semibold text-gray-500'>Doluluk</div>
+      </div>
+      <div className='flex gap-6'>
+        <div className='flex flex-col gap-1'>
+          <div className='text-xs font-semibold text-gray-500'>Şu an doluluk:</div>
+          <div>{occupancy.currentOccupancy}</div>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <div className='text-xs font-semibold text-gray-500'>Toplam kapasite:</div>
+          <div>{occupancy.total}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OccupancyCard;
