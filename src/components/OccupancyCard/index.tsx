19328c5058612659c03926e5ef49f1d5645df365
Efe Kahyaoğlu <75262924+Efekahya@users.noreply.github.com>
2024-05-05 22:22:37 +0300

    feat: current occupancy

--- src/components/OccupancyCard/index.tsx
+++ src/components/OccupancyCard/index.tsx
@@ -1,6 +1,6 @@
-"use client";
+import { getCurrentOccupancy } from "@/api";
+import { getOccupancy } from "@/graphql/occupancy/query";
 import React, { useEffect, useState } from "react";
-import { getOccupancy } from "../../../utils/getOccupancy";
 
 type Occupancy = {
   total: string;
@@ -8,50 +8,27 @@ type Occupancy = {
   capacity: string;
 };
 
-const OccupancyCard = () => {
-  const [occupancy, setOccupancy] = useState<Occupancy>({
-    total: "0",
-    currentOccupancy: "0",
-    capacity: "0",
-  });
-
-  const formatDate = (date: Date): `${number}.${number}.${number}` => {
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-
-    return `${day}.${month}.${year}` as `${number}.${number}.${number}`;
-  };
-
-  useEffect(() => {
-    const fetchOccupancy = async () => {
-      const date = formatDate(new Date());
-      const occupancy = await getOccupancy(date, 0);
-      setOccupancy(occupancy);
-    };
-
-    fetchOccupancy();
-    const intervalId = setInterval(fetchOccupancy, 60 * 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+const OccupancyCard = async () => {
+  const today = new Date();
+  const [year, month, day] = [
+    today.getFullYear(),
+    today.getMonth() + 1,
+    today.getDate(),
+  ];
+  const occupancy = await getCurrentOccupancy();
 
   return (
-    <div className='rounded-xl p-2 px-4 shadow-md outline outline-1 outline-gray-300'>
+    <div className='out rounded-xl p-2 px-4 shadow-md'>
       <div className='mb-2 flex flex-col gap-1'>
-        <div className='text-lg font-semibold text-gray-500'>Doluluk</div>
+        <div className='text-lg font-semibold '>Doluluk</div>
       </div>
       <div className='flex gap-6'>
         <div className='flex flex-col gap-1'>
-          <div className='text-xs font-semibold text-gray-500'>
-            Şu an doluluk:
-          </div>
+          <div className='text-xs font-semibold '>Doluluk:</div>
           <div>{occupancy.currentOccupancy}</div>
         </div>
         <div className='flex flex-col gap-1'>
-          <div className='text-xs font-semibold text-gray-500'>
-            Toplam kapasite:
-          </div>
+          <div className='text-xs font-semibold '>Gün toplam:</div>
           <div>{occupancy.total}</div>
         </div>
       </div>
