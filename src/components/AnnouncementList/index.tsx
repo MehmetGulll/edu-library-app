cbac4d08132e2d8281d47b863af6acb06cfc9d32
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-05-12 21:53:54 +0300

    fix: announcement card style

--- src/components/AnnouncementList/index.tsx
+++ src/components/AnnouncementList/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import getLatestAnnouncements from "../../../utils/getLatestAnnouncements";
 import AnnouncementCard from "../AnnouncementCard";
+import Card from "../Card";
 
 const AnnouncementList = async () => {
   const { announcements } = await getLatestAnnouncements();
-  const colors = ["#7F00FF", "#0F52BA", "#0096FF", "#FF69B4", "#FF5F15"];
+  const colors = [ "#DCFCE7", "#F3E8FF", "#FFE2E5", "#FFF4DE",'#CDe7ff'];
   let colorIndex = 0;
   return (
+    <Card>
     <div id='announcements'>
       <div className='flex justify-center'>
-        <h2 className='text-2xl font-bold text-rose_pompadour-500'>
+        <h2 className='text-xl font-bold text-[#05004E]'>
           Duyurular
         </h2>
       </div>
+      
 
-      <div className='flex flex-wrap gap-4'>
+     
+      <div className='flex flex-wrap gap-8 mt-10'>
         {announcements.map(({ title, description, link, date, content }) => {
           const color = colors[colorIndex];
           colorIndex = (colorIndex + 1) % colors.length;
@@ -32,6 +36,7 @@ const AnnouncementList = async () => {
         })}
       </div>
     </div>
+    </Card>
   );
 };
 
